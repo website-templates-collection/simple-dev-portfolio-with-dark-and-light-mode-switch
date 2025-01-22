143c604c29746b8a83a38a2f6abc8b2a177c3cbc
navctns <navsoft18@gmail.com>
2025-01-22 23:52:22 +0530

    light dark theme switch buttons added

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,20 @@
 // theme switching
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const sunDarkThemeSwitchEl = document.querySelector('.sun-fill-dark');
+const sunLightThemeSwitchEl = document.querySelector('.sun-fill-light');
 
 function switchTheme(e) {
     console.log('switch theme');
     if (e.target.checked) {
         console.log('checked');
         document.documentElement.setAttribute('data-theme', 'dark');
+        sunDarkThemeSwitchEl.styles.display = 'none';
+        sunLightThemeSwitchEl.styles.display = 'block';
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
+        sunDarkThemeSwitchEl.styles.display = 'block';
+        sunLightThemeSwitchEl.styles.display = 'none';
     }    
 }
 
